15b815a1864da5e320eb7f614429bedcad9b4093
silvanagv <silvanagoberdhanvigle@gmail.com>
2016-07-26 17:08:16 -0400

    completed methods lab

---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+# Think of a class like the blueprint that defines how to build an object.
+# The Dog class is different from an individual dog just as the blueprints that show how to build a house are not the actual house.
+# A Ruby class both contains the instructions for creating new objects and has the ability to create those objects.
+# Calling .new on the Dog class is like getting a brand new dog object from an assembly line
+# which produces a series of similar dog objects based on the same Dog template.
+
+
+
+class Person
+
+end
+
+adele_goldberg = Person.new
+
+alan_kay = Person.new
